84390c1451e74875cd3e92844b8bd23467024e4d
Alberto Guerra <aguer273@fiu.edu>
2024-11-08 12:16:20 -0500

    Removed import curbside

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,10 @@
-import Curbside from "./curbside/page";
 
 export default function Home() {
   return (  
   <div>
      <h1>Welcome to Recyclepedia</h1>
      <p>Your go-to resource for recycling in Miami-Dade County.</p>
+
   </div>
   );
 }
