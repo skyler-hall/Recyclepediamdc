1a2dbd6ca2f43390e2089d3738d5c482a9892109
bineta-d <124300302+bineta-d@users.noreply.github.com>
2024-11-08 11:07:02 -0500

    updated section layouts(titles, get involved, watch & learn, footer)

--- app/learn/page.jsx
+++ app/learn/page.jsx
@@ -1,15 +1,16 @@
 "use client";
-
+import '../globals.css';
 import Navbar from "../components/Navbar";
 import React, {useEffect, useState} from 'react';
+import Footer from '../components/Footer';
 
 
 
 function Learn() {
   const games = [
-    { title: "The United Nations Activities", imageSrc: "..." },
-    { title: "Climate Kids Game", imageSrc: "..." },
-    { title: "Environmental Protection Agency Quiz", imageSrc: "..." }
+    { title: "The United Nations Activities", imageSrc: "/img/un-logo.png", link: "" },
+    { title: "Climate Kids Game", imageSrc: "/img/ckids-logo.png" , link: ""},
+    { title: "Environmental Protection Agency Games", imageSrc: "/img/EPA-logo.png", link: "https://www.epa.gov/students/games-quizzes-and-videos-about-environment" }
   ];
 
   const [currentIndex, setCurrentIndex] = useState(0);
@@ -28,11 +29,14 @@ function Learn() {
   return (<div className="">
     <Navbar/>
     {/* FIrst Section  */}
-      <div className="grid  grid-cols-1 sm:grid-cols-2 flex gap-5 justify-center items-center mt-5" >
+    <div className="relative mt-5">
+        <h1 className="text-4xl font-bold text-center w-full p-4 bg-[#234E13] text-white">
+          Learn Page
+        </h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 flex flex-1 justify-center items-center mt-5" >
         {/* left  */}
         <div  className="learn-note text-center sm:text-left p-4 " >
-        <strong><h1 className="text-xl"> Learn Page</h1></strong>
-        <p className="">
+          <p className="">
           Recycling is an essential practice that helps reduce waste, conserve natural resources,
            and minimize the environmental impact of human activity. By turning materials like plastic, 
            paper, glass, and metals into new products, recycling prevents these items from ending up in
@@ -51,7 +55,7 @@ function Learn() {
           <div
             className="flex transition-transform duration-700 ease-in-out"
             style={{ transform: `translateX(-${currentIndex * 100}%)` }}
-          >
+            >
             {games.map((game, index) => (
               <div
                 key={index}
@@ -64,20 +68,126 @@ function Learn() {
                   // minWidth: '200px',
                 }}
               >
-                <img src={game.imageSrc} alt={game.title} className="w-full h-48 object-cover" />
+                 <a 
+               key={index} 
+                href={game.link} 
+                className="card text-center p-4 min-w-full"
+                style={{
+                  // border: '2px solid grey',
+                  borderRadius: '10px',
+                  padding: 'auto 20px',
+                  textDecoration: 'none' 
+               }}
+                >
+                <img src={game.imageSrc} alt={game.title} className="w-full h-48  object-center" />
                 <div className="card-body">
                   <p>{game.title}</p>
                   <hr />
-                  <button type="button" className="text-white bg-green-500 hover:bg-green-700 mt-2 font-medium rounded-lg text-sm px-5 py-2.5">
-                    Play Game!
-                  </button>
+                  {/* <button type="button" className="text-white bg-green-500 hover:bg-green-700 mt-2 font-medium rounded-lg text-sm px-5 py-2.5">
+                    Play Games!
+                  </button> */}
                 </div>
+                </a>
               </div>
+              
             ))}
           </div>
         </div>
+        </div>
+
+
+          
+              {/* Second section */}
+              <div className="relative mt-10">
+        <h1 className="text-4xl font-bold text-center w-full p-4 bg-[#234E13] text-white">
+          Get Involved
+        </h1>
+              <div className="grid md:grid-rows-1  sm:grid-cols-1 md:grid-cols-3 gap-6 mt-6 w-screen p-5">
+
+                <div className="max-w-full w-full rounded overflow-hidden shadow-lg">
+                <a href="https://volunteercleanup.org/" className="">
+
+                  <img className="w-full h-48 object-cover" src="/img/involved1.png" alt="Sunset in the mountains"/></a>
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">Volunteer Clean Up</div>
+                    <p className="text-gray-700 text-base">
+                    VolunteerCleanup.Org engages volunteers to cleanup their neighborhoods, shorelines, and waterways as a way to raise awareness about marine debris and encourage people to reduce their reliance on single-use plastics.
+                    </p>
+                  </div>
+                  <div className="px-6 pt-4 pb-2">
+                    {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Volunteer</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Recycle</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HandsOn</span> */}
+                  </div>
+                </div>
+
+
+                {/* card 2 */}
+
+                <div className="max-w-full w-full  rounded overflow-hidden shadow-lg">
+                <a href="https://www.handsonmiami.org/about-membership?layoutViewMode=tablet" className="">
+
+                  <img className="w-full h-48 object-cover" src="/img/composting.webp" alt="Sunset in the mountains"/></a>
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">Composting</div>
+                    <p className="text-gray-700 text-base">
+                    Composting is the process of turning organic waste into nutrient-rich soil through natural decomposition. It's an eco-friendly way to reduce waste and enrich soil.
+                  <br/> Get started with the resource below! </p>
+                    <a  target="_blank" href="https://www.miamidade.gov/global/solidwaste/home-composting.page" className="underline">Home Composting</a> <br></br>
+                    <a  target="_blank" href="https://sfyl.ifas.ufl.edu/sarasota/natural-resources/waste-reduction/composting/" className="underline">UF/IFAS Composting</a>
+                  </div>
+                  <div className="px-6 pt-4 pb-2">
+                    {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Volunteer</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Compost</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HandsOn</span> */}
+                  </div>
+                </div>
+
+                {/* card 3 */}
+
+                <div className="max-w-full w-full  rounded overflow-hidden shadow-lg">
+                <a href="https://www.handsonmiami.org/about-membership?layoutViewMode=tablet" className="">
+
+                  <img className="w-full h-48  object-center" src="/img/handson.jpeg" alt="Sunset in the mountains"/></a>
+                  <div className="px-6 py-4">
+                    <div className="font-bold text-xl mb-2">Hands On Miami</div>
+                    <p className="text-gray-700 text-base">
+                     Become a volunteer & make difference in the community!
+                    </p>
+                  </div>
+                  <div className="px-6 pt-4 pb-2">
+                    {/* <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Volunteer</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Recycle</span>
+                    <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#HandsOn</span> */}
+                  </div>
+                </div>
+              
+         </div>
+        </div>
+        </div>
+
+       {/* section 3 */}
+
+      <div className="relative mt-10">
+          <h1 className="text-4xl font-bold text-center w-[30%] p-4 bg-[#234E13] text-white">
+             Watch and Learn
+           </h1>
+  
+      <div className="video-grid flex flex-col gap-6 justify-center mt-6 w-[30%] px-4">
+        <div className="video flex justify-content-center w-full rounded-lg overflow-hidden shadow-lg">
+          <iframe width="100%" height="315" src="https://www.youtube.com/embed/Fex-wvrOZf4?si=nfK5j6JH8LKNTeic" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy='strict-origin-when-cross-origin' allowFullScreen></iframe>
+        </div>
+
+        <div className="video flex justify-center w-full rounded-lg overflow-hidden shadow-lg">
+        <iframe width="100%" height="315" src="https://www.youtube.com/embed/cNPEH0GOhRw?si=5REcFjBYd5PPpjms" title="YouTube video player" frameBorder={0} allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy='strict-origin-when-cross-origin' allowFullScreen></iframe>     </div>
+      
+       </div>
+
       </div>
+      
+      <Footer/>
     </div>
+    
   );
 }
 
