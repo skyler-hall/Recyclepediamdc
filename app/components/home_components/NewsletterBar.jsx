0db12f166303d142664f5b48b7e09d791c4adea4
Roseline-Projects <rdami007@fiu.edu>
2024-11-22 01:31:31 -0500

    css fixes

--- app/components/home_components/NewsletterBar.jsx
+++ app/components/home_components/NewsletterBar.jsx
@@ -27,13 +27,14 @@ const NewsletterBar = () => {
         //rest...
     }
 
+    //-mx-4 md:-mx-20
     return (
-        <div className="bg-[#a9def9] flex p-4 py-8 px-2 -mx-4 md:-mx-20 -mt-8 -mb-8 lg:-mx-12">
-            <h2 className="text-l sm:text-3xl md:text-5xl lg:text-6xl text-left text-black w-2/5 border-r-2 md:border-r-4 border-[#DBF4D2] px-2 md:ml-10 lg:pr-4">Subscribe to our Newsletter</h2>
-            <form onSubmit={handleSubmit} className='w-3/5 px-2 md:px-8 m-auto'>
+        <div className="bg-[#a9def9] flex py-8 w-screen relative left-[calc(-50vw+50%)] xl:left-0 -mt-8 -mb-8 left-0 max-w-7xl">
+            <h2 className="text-l sm:text-3xl md:text-5xl lg:text-6xl text-left text-black w-2/5 border-r-2 md:border-r-4 border-[#DBF4D2] px-2 pl-3 md:px-4 md:ml-10 lg:pr-4">Subscribe to our Newsletter</h2>
+            <form onSubmit={handleSubmit} className='w-3/5 px-2 md:pl-6 m-auto'>
                 <label className="text-xs sm:text-xl md:text-2xl"> Email:
                     <input 
-                    className={`rounded-2xl w-1/3 sm:w-1/2 md:w-5/12 lg:w-7/12 mx-1 md:mx-4 py-1 pl-2 md:pl-4`}
+                    className={`rounded-2xl mx-1 w-1/3 sm:w-1/2 md:w-5/12 lg:w-7/12 md:mx-4 py-1 pl-2 md:pl-4`}
                     type='email' 
                     value={emailInput}
                     onChange={handleInput} 
