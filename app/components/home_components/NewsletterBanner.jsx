bf75b7bc5cda03c438c47a8c4ef94246d64e11e5
Roseline-Projects <rdami007@fiu.edu>
2024-12-06 11:43:46 -0500

    final css fixes

--- app/components/home_components/NewsletterBanner.jsx
+++ app/components/home_components/NewsletterBanner.jsx
@@ -6,9 +6,9 @@ import Link from "next/link";
 
 const NewsletterBanner = () => {
     return(
-        <div className="bg-[#234E13] rounded-3xl shadow-2xl sm:flex items-center w-screen relative left-[calc(-50vw+50%)] xl:left-0 py-8 my-40 2xl:mt-20 max-w-7xl">
+        <div className="bg-[#234E13] rounded-3xl shadow-2xl sm:flex items-center w-screen relative left-[calc(-50vw+50%)] xl:left-0 py-8 my-30 2xl:mt-20 max-w-7xl">
             <div className="sm:w-4/12 mb-6 sm:mb-0">
-                <img src='Diggy.png' className=" scale-110 md:scale-125"/>
+                <img src='Diggy.png' className=" scale-110 md:scale-125" alt="Diggy"/>
             </div>
             <div className="mx-8 p-4 sm:w-8/12 justify-self-end sm:p-0 sm:pr-8">
                 <h2 className="text-xl sm:text-3xl md:text-5xl lg:text-6xl font-bold text-left text-white mb-4">Subscribe to our Newsletter</h2>
