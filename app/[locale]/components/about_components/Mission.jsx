dcef527a64e76a034d8faf1fbd7f3e47bdfcc777
raglandconnor <connor@rglnd.com>
2024-12-17 23:44:49 -0500

    Add: translation to mission on about page

--- app/[locale]/components/about_components/Mission.jsx
+++ app/[locale]/components/about_components/Mission.jsx
@@ -1,7 +1,10 @@
 import Image from 'next/image';
 import { useState, useRef, useEffect } from 'react';
+import { useTranslations } from 'next-intl';
 
 function Mission() {
+  const t = useTranslations('AboutPage.ourStory.mission');
+
   const [isExpanded, setIsExpanded] = useState(false);
   const [isInView2, setIsInView2] = useState(false);
   const imageRef2 = useRef(null);
@@ -35,45 +38,22 @@ function Mission() {
   return (
     <section className='flex flex-col lg:flex-row-reverse items-center p-8 lg:p-16 bg-white text-center'>
       <div className='lg:w-1/2 p-0 md:p-4'>
-        <h2 className='text-2xl font-semibold text-darkGreen'>Our Mission</h2>
-        <p className='mt-4 text-gray-700 text-lg'>
-          At Dream in Green (DIG), we recognize that contamination is one of the
-          biggest challenges in recycling, occurring when non-recyclable items
-          enter the recycling system. This not only diminishes recycling success
-          rates but also creates confusion among residents regarding acceptable
-          materials in their local programs.
-        </p>
+        <h2 className='text-2xl font-semibold text-darkGreen'>{t('title')}</h2>
+        <p className='mt-4 text-gray-700 text-lg'>{t('mainContent')}</p>
 
-        {/* Conditionally render additional content */}
         {isExpanded && (
           <div className='mt-4 text-gray-700'>
-            <p>
-              To address these challenges, our mission is to provide Miami-Dade
-              County residents with accessible and up-to-date recycling
-              information. By offering clear guidance on proper recycling
-              practices, we empower individuals to participate effectively in
-              their community's recycling efforts, ultimately increasing the
-              recovery of recyclables.
-            </p>
-            <p>
-              <br />
-              Our platform, <strong>Recyclepedia</strong>, serves as a
-              comprehensive resource for accurate information on what can be
-              recycled. It helps users understand acceptable items in their
-              municipalities and provides alternative disposal options for those
-              not accepted in their recycling programs. By facilitating proper
-              recycling practices, Recyclepedia contributes to higher recycling
-              rates, reduced pollution, and improved community health.
-            </p>
+            <p>{t('expandedContent1')}</p>
+            <br />
+            <p>{t('expandedContent2')}</p>
           </div>
         )}
 
-        {/* Toggle button */}
         <button
           onClick={toggleExpansion}
           className='mt-6 px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700'
         >
-          {isExpanded ? 'Show Less' : 'Read More'}
+          {isExpanded ? t('button.less') : t('button.more')}
         </button>
       </div>
 
