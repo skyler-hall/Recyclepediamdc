60a41fe835083a19792d446e1a0d0dbb552658ef
raglandconnor <connor@rglnd.com>
2024-12-17 23:41:08 -0500

    Add: translation to OurStory on about page

--- app/[locale]/components/about_components/OurStory.jsx
+++ app/[locale]/components/about_components/OurStory.jsx
@@ -1,8 +1,10 @@
 import Image from 'next/image';
 import { Link } from '@/i18n/routing';
 import { useState, useRef, useEffect } from 'react';
+import { useTranslations } from 'next-intl';
 
 function OurStory() {
+  const t = useTranslations('AboutPage.ourStory');
   const [isInView1, setIsInView1] = useState(false);
   const imageRef1 = useRef(null);
 
@@ -34,7 +36,7 @@ function OurStory() {
         <Image
           ref={imageRef1}
           src='/OurStory.jpg'
-          alt='Our Story'
+          alt={t('title')}
           width={400}
           height={300}
           className={`curved-image-right rounded-lg transition-transform duration-700 ease-out ${
@@ -44,20 +46,11 @@ function OurStory() {
       </div>
 
       <div className='lg:w-1/2 p-2 sm:p-4'>
-        <h1 className='text-3xl font-bold text-darkGreen'>Our Story</h1>
-        <p className='mt-4 text-gray-700 text-lg'>
-          Since 2006, Dream in Green (DIG) has empowered individuals, especially
-          youth, to take a leading role in addressing climate change and other
-          environmental challenges facing South Florida. By assisting schools,
-          households, local governments, and businesses, DIG helps reduce their
-          environmental footprint. Through partnerships within our community, we
-          develop, implement, and oversee educational programs and workshops
-          that promote environmentally sustainable behaviors among all age
-          groups, with a particular emphasis on K-12 students.
-        </p>
+        <h1 className='text-3xl font-bold text-darkGreen'>{t('title')}</h1>
+        <p className='mt-4 text-gray-700 text-lg'>{t('content')}</p>
         <Link href='/learn'>
           <button className='mt-6 px-6 py-2 bg-green-600 text-white rounded hover:bg-green-700'>
-            Learn More
+            {t('learnMore')}
           </button>
         </Link>
       </div>
