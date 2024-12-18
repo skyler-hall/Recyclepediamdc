7d1973d145dbac8ba2642a78d23579375f53c9b4
raglandconnor <connor@rglnd.com>
2024-12-17 23:52:38 -0500

    Add: translation to footer links

--- app/[locale]/components/Footer.jsx
+++ app/[locale]/components/Footer.jsx
@@ -1,44 +1,78 @@
-import React from "react";
-import Link from "next/link"
-import Image from "next/image";
+import React from 'react';
+import { Link } from '@/i18n/routing';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaLinkedin,
+  FaXTwitter,
+} from 'react-icons/fa6';
+import { useTranslations } from 'next-intl';
 
+function Footer() {
+  const t = useTranslations('Footer');
 
-function Footer () {
-    return (
-      <footer className="w-full bg-[#234E13] text-black py-2">
-      <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+  return (
+    <footer className='w-full bg-[#234E13] text-white py-2'>
+      <div className='container mx-auto flex flex-col md:flex-row items-center justify-between'>
         {/* Copyright Text */}
-        <div className="text-center md:text-left mb-4 md:mb-0">
+        <div className='text-center md:text-left mb-4 md:mb-0'>
           <p>© 2024 Recyclepedia</p>
         </div>
-        
+
         {/* Navigation Links */}
-        <div className="flex space-x-4 mb-4 md:mb-0">
-          <Link href="/curbside" className="hover:underline">Curbside</Link>
-          <Link href="/items" className="hover:underline">Items</Link>
-          <Link href="/learn" className="hover:underline">Learn</Link>
-          <Link href="/about" className="hover:underline">About</Link>
+        <div className='flex space-x-4 mb-4 md:mb-0'>
+          <Link href='/curbside' className='hover:underline'>
+            {t('nav.curbside')}
+          </Link>
+          <Link href='/items' className='hover:underline'>
+            {t('nav.items')}
+          </Link>
+          <Link href='/learn' className='hover:underline'>
+            {t('nav.learn')}
+          </Link>
+          <Link href='/about' className='hover:underline'>
+            {t('nav.about')}
+          </Link>
         </div>
 
         {/* Social Media Icons */}
-        <div className="flex space-x-4">
-          <a href="https://www.facebook.com/dreamingreen" target="_blank" rel="noopener noreferrer" aria-label="Facebook">
-            <Image src="/FacebookLogo.png" alt="Facebook Logo" width={18} height={18} />
-          </a>
-          <a href="https://www.instagram.com/dreamingreenmia/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
-            <Image src="/InstagramLogo.png" alt="Instagram Logo" width={18} height={18} />
-          </a>
-          <a href="https://www.linkedin.com/company/dream-in-green/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
-            <Image src="/LinkedInLogo.png" alt="LinkedIn Logo" width={18} height={18} />
-          </a>
-          <a href="https://x.com/Dream_in_Green" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
-            <Image src="/XLogo.png" alt="Twitter Logo" width={18} height={18} />
-          </a>
+        <div className='flex space-x-4'>
+          <Link
+            href='https://www.facebook.com/dreamingreen'
+            target='_blank'
+            rel='noopener noreferrer'
+            aria-label='Facebook'
+          >
+            <FaFacebook size={20} />
+          </Link>
+          <Link
+            href='https://www.instagram.com/dreamingreenmia/'
+            target='_blank'
+            rel='noopener noreferrer'
+            aria-label='Instagram'
+          >
+            <FaInstagram size={20} />
+          </Link>
+          <Link
+            href='https://www.linkedin.com/company/dream-in-green/'
+            target='_blank'
+            rel='noopener noreferrer'
+            aria-label='LinkedIn'
+          >
+            <FaLinkedin size={20} />
+          </Link>
+          <Link
+            href='https://x.com/Dream_in_Green'
+            target='_blank'
+            rel='noopener noreferrer'
+            aria-label='Twitter'
+          >
+            <FaXTwitter size={20} />
+          </Link>
         </div>
       </div>
     </footer>
-    );
-  };
-  
-  export default Footer;
-  
+  );
+}
+
+export default Footer;
